393411b9bc593f3e6fd1130021c84aa05697e31c
LOL <146339641+uiop123456@users.noreply.github.com>
2024-02-03 02:57:53 +0530

    Update 121. Best Time to Buy and Sell Stock.cpp

--- 121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -11,3 +11,27 @@ public:
         return ans;
     }
 };
+
+// better most probaby 
+// if you have the power of predicting the future then why not use it perfectly by adding a dummy array and make a count of max_so_far in reverse order traversal
+class Solution {
+public:
+    int maxProfit(vector<int>& nums) {
+        vector<int> dum(nums.size(), 0);
+        int max_so_far = 0;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            max_so_far = max(max_so_far, nums[i]);
+            dum[i] = max(max_so_far, dum[i]);
+        }
+
+        int ans = 0;  
+        
+        for (int i = 0; i < nums.size(); i++) {
+            ans = max(ans, dum[i] - nums[i]);
+        }
+
+        return ans;
+    }
+};
+
