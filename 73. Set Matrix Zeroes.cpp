bb7e94efc6c08713afec479ad4137d4f3e0943c5
LOL <146339641+uiop123456@users.noreply.github.com>
2024-02-02 13:38:34 +0530

    Create 73. Set Matrix Zeroes.cpp

---
+++ 73. Set Matrix Zeroes.cpp
@@ -0,0 +1,27 @@
+// mid
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(); // row
+        int n = matrix[0].size(); // col
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
