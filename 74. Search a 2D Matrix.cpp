033fb5b33c73ffaaabfa63b34352ece5a67fbaad
LOL <146339641+uiop123456@users.noreply.github.com>
2024-02-05 23:00:04 +0530

    Update 74. Search a 2D Matrix.cpp

--- 74. Search a 2D Matrix.cpp
+++ 74. Search a 2D Matrix.cpp
@@ -12,3 +12,48 @@ public:
         return false;
     }
 };
+// kinda binary search
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& nums, int target) {
+        int row=nums.size();
+        int col=nums[0].size();
+        int i=0,j=col-1;
+        // i ----> <-----j(upside down)
+        while(i<row && j>=0){
+            if(nums[i][j]==target) return true;
+            else if(nums[i][j]>target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
+
+// binary nlogn
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& nums, int target) {
+        int row=nums.size();
+        int col=nums[0].size();
+
+        int start=0;
+        int end=row*col-1;
+        
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            int row_formula = mid/col;
+            int col_formula = mid%col;
+
+            if(nums[row_formula][col_formula]<target){
+                start=mid+1;
+            }else if(nums[row_formula][col_formula]>target){
+                end=mid-1;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
