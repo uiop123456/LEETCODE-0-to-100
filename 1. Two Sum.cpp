693823726f74736e907f8a0530c50b44ad9a3b30
LOL <146339641+uiop123456@users.noreply.github.com>
2024-02-20 19:23:35 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -10,3 +10,16 @@ public:
         return {};
     }
 };
+// umap
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>umap;
+        for(int i=0;i<nums.size();i++){
+            if(umap.find(target-nums[i])!=umap.end()) return {i,umap[target-nums[i]]};
+            umap[nums[i]]=i;
+        }
+        return {};
+    }
+};
+
