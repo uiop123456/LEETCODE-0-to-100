af2bbf6b72c600b07224ac744429caf79433ffc2
LOL <146339641+uiop123456@users.noreply.github.com>
2024-02-20 18:36:50 +0530

    Update 493. Reverse Pairs.cpp

--- 493. Reverse Pairs.cpp
+++ 493. Reverse Pairs.cpp
@@ -11,3 +11,49 @@ public:
         return ans;
     }
 };
+
+// merge sort
+class Solution {
+public:
+    void merge(vector<int>&nums,int low,int mid,int high,int &ans){ // pass with address
+        int j=mid+1;
+        for(int i=low;i<=mid;i++){
+            while(j<=high && nums[i] > 2*(long long)nums[j]){
+                j++;
+            }
+            ans = ans + j - (mid+1);
+        }
+        int size = high-low+1;
+        vector<int>temp(size,0);
+        int left=low,right=mid+1,k=0;
+        while(left<=mid && right<=high){
+            if(nums[left] < nums[right])
+                temp[k++] = nums[left++];
+            else
+                temp[k++] = nums[right++];
+        }
+        while(left<=mid){
+            temp[k++]=nums[left++];
+        }
+        while(right<=high)
+            temp[k++]=nums[right++];
+        int m=0;
+        for(int i=low;i<=high;i++){
+            nums[i] = temp[m++];
+        }
+    }
+    void mergesort(vector<int>&nums,int low,int high,int &ans){
+        if(low>=high){
+            return;
+        }
+        int mid= low+(high-low)/2;
+        mergesort(nums,low,mid,ans);
+        mergesort(nums,mid+1,high,ans);
+        merge(nums,low,mid,high,ans);
+    }
+    int reversePairs(vector<int>& nums) {
+        int ans=0;
+        mergesort(nums,0,nums.size()-1,ans);
+        return ans;
+    }
+};
