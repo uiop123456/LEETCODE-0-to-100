3e42beecad8fac419907cbdc8ebc06e3a618218a
LOL <146339641+uiop123456@users.noreply.github.com>
2024-02-20 18:59:56 +0530

    Create 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,12 @@
+// the worst of all and the blood run stale
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size()-1;i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i] + nums[j] == target) return {i,j};
+            }
+        }
+        return {};
+    }
+};
