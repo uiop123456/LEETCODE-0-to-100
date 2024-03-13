65b046c71640a36ea0bf349dd4c9bb742710a6f1
LOL <146339641+uiop123456@users.noreply.github.com>
2024-03-14 03:58:52 +0530

    Create 3005. Count Elements With Maximum Frequency.cpp

---
+++ 3005. Count Elements With Maximum Frequency.cpp
@@ -0,0 +1,37 @@
+//double pass
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        vector<int>ans(101,0);
+        int maxF=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            ans[nums[i]]++;
+            maxF=max(maxF,ans[nums[i]]);
+        }
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]==maxF)count++; 
+        }
+        return count*maxF;
+    }
+};
+
+//single pass
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int maxF=0;
+        int count=0;
+        vector<int>ans(101,0);
+        for(int i=0;i<nums.size();i++){
+            ans[nums[i]]++;
+            if(ans[nums[i]]>maxF){
+                maxF=ans[nums[i]];
+                count=maxF;
+            }else if(ans[nums[i]]==maxF){
+                count+=maxF;
+            }
+        }
+        return count;
+    }
+};  
