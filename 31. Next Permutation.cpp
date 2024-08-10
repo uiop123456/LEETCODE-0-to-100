2c853b1199c3ca6d695712fae8f65b109c371eae
LOL <146339641+uiop123456@users.noreply.github.com>
2024-08-11 02:55:00 +0530

    Update 31. Next Permutation.cpp

--- 31. Next Permutation.cpp
+++ 31. Next Permutation.cpp
@@ -29,3 +29,34 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        if(nums.size()==1) return;
+        int first=0,second=nums.size()-1,n=nums.size()-1;
+        int i=0;
+        bool flag=true;
+        for(i=nums.size()-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                first = i;
+                flag=false;
+                break;
+            }
+        }
+        if(flag) return reverse(nums.begin(),nums.end());
+        while(first<second){
+            if(nums[first]<nums[second]){
+                break;
+            }
+            second--;
+        }
+        swap(nums[first],nums[second]);
+        second=first+1;
+        while(second<n){
+            swap(nums[second],nums[n]);
+            second++;n--;
+        }
+        return;
+    }
+};
