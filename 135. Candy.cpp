ccd3f832cb7afe29ae4ae627ca1f4b2f278117a3
LOL <146339641+uiop123456@users.noreply.github.com>
2024-08-11 01:45:39 +0530

    Update 135. Candy.cpp

--- 135. Candy.cpp
+++ 135. Candy.cpp
@@ -17,3 +17,38 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int candy(vector<int>& nums) {
+        int n = nums.size();
+        int ans=n; //everyone must have 1 candy so init all by 1
+        int i=1;
+        while(i<n){
+            // going flat
+            if(nums[i]==nums[i-1]){
+                i++;
+                continue;
+            }
+
+            // going up
+            int up=0;
+            while(nums[i]>nums[i-1]){
+                up++;
+                ans+=up;
+                i++;
+                if(i==nums.size()) return ans; //if only monotonic increase array
+            }
+            // goind down - monotonic decreasing array
+            int down = 0;
+            while(i<n  && nums[i]<nums[i-1]){
+                down++;
+                ans += down;
+                i++;
+            }
+            // jeta extra add hochilo oita minus korte hobe
+            ans -= min(up,down);
+        }
+        return ans;
+    }
+};
