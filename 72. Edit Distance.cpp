13663e3baf0655b4078be9b23831babf1d6be298
LOL <146339641+uiop123456@users.noreply.github.com>
2024-08-14 14:08:08 +0530

    Create 72. Edit Distance.cpp

---
+++ 72. Edit Distance.cpp
@@ -0,0 +1,50 @@
+// L2R
+class Solution {
+public:
+    int solve(string f, string s, int i, int j, vector<vector<int>> &dp){
+        if(j == s.size()) return f.size() - i;
+        if(i == f.size()) return s.size() - j;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if(f[i] == s[j]){
+            return solve(f, s, i + 1, j + 1, dp);
+        }
+
+        int insert = solve(f, s, i, j + 1, dp);
+        int del = solve(f, s, i + 1, j, dp);
+        int repl = solve(f, s, i + 1, j + 1, dp);
+
+        return dp[i][j] = 1 + min(insert, min(del, repl));
+    }
+
+    int minDistance(string f, string s) {
+        vector<vector<int>> dp(500,vector<int>(500,-1));
+        return solve(f, s, 0, 0, dp);
+    }
+};
+
+// R2L
+class Solution {
+public:
+    int solve(string &f, string &s,int i,int j, vector<vector<int>> &dp){
+        if (i < 0) return j + 1;
+        if (j < 0) return i + 1;
+
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        if (f[i] == s[j]) {
+            return dp[i][j] = solve(f, s, i - 1, j - 1,dp);
+        } 
+
+        int insert = solve(f, s, i, j - 1, dp);
+        int delet = solve(f, s, i - 1, j, dp);
+        int replace = solve(f, s, i - 1, j - 1, dp);
+
+        return dp[i][j] = 1 + min(insert, min(delet, replace));
+    }
+    int minDistance(string f, string s) {
+        vector<vector<int>> dp(f.size(),vector<int>(s.size(),-1));
+        return solve(f,s,f.size()-1,s.size()-1,dp);
+    }
+};
