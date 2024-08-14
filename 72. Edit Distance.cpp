94e5225d2bf1cc3868c7ca5060299a6b9992b44e
LOL <146339641+uiop123456@users.noreply.github.com>
2024-08-14 14:37:47 +0530

    Update 72. Edit Distance.cpp

--- 72. Edit Distance.cpp
+++ 72. Edit Distance.cpp
@@ -48,3 +48,23 @@ public:
         return solve(f,s,f.size()-1,s.size()-1,dp);
     }
 };
+
+// btm up
+class Solution {
+public:
+    int minDistance(string s, string t) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1));
+
+        for(int i=0;i<=s.size();i++){
+            for(int j=0;j<=t.size();j++){
+                if(i==0 || j==0){
+                    dp[i][j] = i+j; 
+                }else if(s[i-1] == t[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else
+                    dp[i][j] = 1 + min({dp[i][j-1], dp[i-1][j], dp[i-1][j-1]});
+            }
+        }
+        return dp[s.size()][t.size()];
+    }
+};
